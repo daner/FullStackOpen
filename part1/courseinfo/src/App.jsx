19a44e46a80270fdc7bbfa069623e9fcf345188e
Daniel Eriksson <daniel.j.eriksson@consid.se>
2024-07-29 15:14:40 +0200

    Work

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,17 +1,24 @@
 const App = () => {
-  const course = 'Half Stack application development'
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
+    ],
+    calculateTotal: function() {
+      let sum = 0;
+      this.parts.forEach(x => sum += x.exercises);
+      return sum;
+    }
+  }
 
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 }
-  ]
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total course={course} />
     </div>
   )
 }
@@ -42,7 +49,7 @@ const Part = (props) => {
 
 const Total = (props) => {
   return(
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    <p>Number of exercises {props.course.calculateTotal()}</p>
   )
 }
 
