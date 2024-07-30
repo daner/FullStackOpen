ba9879adbc6b3e7cefbeb901ae3100c30539efd4
Daniel Eriksson <daniel.j.eriksson@consid.se>
2024-07-30 09:52:40 +0200

    Cleanup

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,23 +2,29 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 })
+
+  const incrementGood = () => {
+    setFeedback({...feedback, good: feedback.good + 1});
+  }
+
+  const incrementNeutral = () => {
+    setFeedback({...feedback, neutral: feedback.neutral + 1});
+  }
+
+  const incrementBad = () => {
+    setFeedback({...feedback, bad: feedback.bad + 1});
+  }
 
   return (
     <>
       <div>
         <Header heading="give feedback" />
-        <button>good</button>
-        <button>neutral</button>
-        <button>bad</button>
-        <h1>statistics</h1>
-        <div>
-          <p>good 6</p>
-          <p>good 2</p>
-          <p>good 1</p>
-        </div>
+        <Button handleClick={incrementGood} name="good" />
+        <Button handleClick={incrementNeutral} name="neutral" />
+        <Button handleClick={incrementBad} name="bad" />
+        <Statistics feedback={feedback} />
       </div>
     </>
   )
@@ -31,11 +37,41 @@ const Header = (props) => {
 }
 
 const Button = (props) => {
-  return <button onClick={props.handleClick}>{props.name}</button>
+  return(
+  <button onClick={props.handleClick}>{props.name}</button>
+)
 }
 
-const Stats = (props) => {
-  
+const Display = (props) => {
+  return(
+    <p>{props.name} {props.value}</p>
+  )
+}
+
+const Statistics = (props) => {
+  const {feedback} = props;
+
+  const calculateSum = () => {
+    return feedback.good + feedback.neutral + feedback.bad;
+  }
+
+  const calculateAverage = () => {
+
+  }
+
+  const calculatePositive = () => {
+
+  }
+
+  return(
+    <>
+      <Header heading="statistics" />
+      <Display name="good" value={feedback.good} />
+      <Display name="neutral" value={feedback.neutral} />
+      <Display name="bad" value={feedback.bad} />
+      <Display name="all" value={calculateSum()} />
+    </>
+  )
 }
 
 export default App
