eb91ea0802c4c5eb04e7dbe4be8a8a84289556a3
Daniel Eriksson <daniel.j.eriksson@consid.se>
2024-07-31 15:49:20 +0200

    Work

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const PORT = 3001
+
+var persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id == id)
+    if(person) {
+        response.json(person)
+    } else{
+        response.status(404).end()
+    }
+})
+
+app.get('/info', (request, response) => {
+    const now = Date.now()
+    const today = new Date(now)
+
+    response.send(`<html><body><p>Phonebook has info for ${persons.length} people</p><p>${today}</p></body></html>`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
