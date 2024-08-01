f621309b93e1c8d0e0105b7de94e6155f9e4a324
Daniel Eriksson <daniel.j.eriksson@consid.se>
2024-08-01 09:43:07 +0200

    Up to 3.8 complete

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const PORT = 3001
+
+app.use(express.json())
+
+morgan.token('body', function (req, res) { 
+    return JSON.stringify(req.body) 
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 var persons = [
     { 
@@ -31,13 +40,63 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    const person = persons.find(person => person.id == id)
+    const person = persons.find(person => person.id === id)
     if(person) {
         response.json(person)
     } else{
         response.status(404).end()
     }
 })
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+    if(person) {
+        persons = persons.filter(item => item.id != person.id)
+        response.status(204).end()
+    } else{
+        response.status(404).end()
+    }
+})
+
+app.post('/api/persons', (request, response) => {
+    
+    const body = request.body
+
+    if (!body.number) {
+        return response.status(400).json({ 
+          error: 'number missing' 
+        })
+    }
+
+    if (!body.name) {
+        return response.status(400).json({ 
+          error: 'name missing' 
+        })
+    }
+
+    const duplicate = persons.find(person => person.name === body.name)
+
+    if(duplicate) {
+        return response.status(400).json({ 
+            error: 'name must be unique' 
+          })    
+    }
+
+    const id = Math.floor(Math.random() * 10000)
+
+    const person = {
+        id: String(id),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
 
 app.get('/info', (request, response) => {
     const now = Date.now()
@@ -46,6 +105,13 @@ app.get('/info', (request, response) => {
     response.send(`<html><body><p>Phonebook has info for ${persons.length} people</p><p>${today}</p></body></html>`)
 })
 
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+app.use(unknownEndpoint)
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
