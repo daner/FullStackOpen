cc5c18025d885f4f50f2263577c13a05bd1f1d6f
Daniel Eriksson <daniel.j.eriksson@consid.se>
2024-08-02 15:00:48 +0200

    Work

--- part4/blog-backend/tests/blog_api.test.js
+++ part4/blog-backend/tests/blog_api.test.js
@@ -2,10 +2,13 @@ const { test, describe, after, beforeEach, before } = require('node:test')
 const _ = require('lodash')
 const assert = require('node:assert')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const blogs = require('./data/blogs')
+const helper = require('./test_helper')
+const bcrypt = require('bcrypt')
 const db = require('../db.js')
 const { MongoDBContainer } = require('@testcontainers/mongodb')
 
@@ -196,6 +199,39 @@ describe("blogs", () => {
       assert.strictEqual(getResult.body.likes, beforeBlog.likes + 1)  
   })
 
+})
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    assert(usernames.includes(newUser.username))
+  })
 })
 
 after(async () => {
