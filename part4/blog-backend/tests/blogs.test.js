eb1c29d5404c6757e4dfc7a7bbc4a7b5d49214f6
Daniel Eriksson <daniel.j.eriksson@consid.se>
2024-08-02 14:23:19 +0200

    Tests

--- part4/blog-backend/tests/blogs.test.js
+++ part4/blog-backend/tests/blogs.test.js
@@ -127,7 +127,75 @@ describe("blogs", () => {
       .send(newBlog)
       .expect(400)
   })
+
+  test("count decrease by one after delete", async () => {
+
+    const beforeResult = await api
+      .get('/api/blogs')
+      .expect(200)
+
+    const beforeCount = beforeResult.body.length
+    const id = beforeResult.body[0].id
+
+    await api.delete(`/api/blogs/${id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const afterResult = await api
+      .get('/api/blogs')
+      .expect(200)
+
+    const afterCount = afterResult.body.length
+
+    assert.strictEqual(beforeCount - 1, afterCount)
+  })
  
+  test("can get added blog", async () => {
+    const newBlog = {
+      title: "Test title",
+      author: "Test author",
+      url: "http://example.com",
+      likes: 4
+    }
+
+    const addedBlog = await api.post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const id = addedBlog.body.id
+
+    const getBlog = await api
+      .get(`/api/blogs/${id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    assert.strictEqual(getBlog.body.title, newBlog.title)  
+    assert.strictEqual(getBlog.body.author, newBlog.author)  
+    assert.strictEqual(getBlog.body.url, newBlog.url)  
+    assert.strictEqual(getBlog.body.likes, newBlog.likes)  
+  })
+
+  test("can update likes and get new object from put", async () => {
+    const allResult = await api
+      .get('/api/blogs')
+      .expect(200)
+
+    const beforeBlog = allResult.body[0]
+
+    const putResult = await api
+      .put(`/api/blogs/${beforeBlog.id}`)
+      .send({likes: beforeBlog.likes + 1})
+      .expect(200)
+
+    const getResult = await api
+      .get(`/api/blogs/${beforeBlog.id}`)
+      .expect(200)
+
+      assert.strictEqual(putResult.body.likes, beforeBlog.likes + 1)  
+      assert.strictEqual(getResult.body.likes, beforeBlog.likes + 1)  
+  })
+
 })
 
 after(async () => {
