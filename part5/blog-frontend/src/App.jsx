afd1770a530e8007819591ca0812ab61a1f761f4
Daniel Eriksson <daniel.j.eriksson@consid.se>
2024-08-05 10:23:35 +0200

    Work

--- part5/blog-frontend/src/App.jsx
+++ part5/blog-frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+
+  const callback = (user) => {
+    setUser(user)
+  }
+
+  const logout = () => {
+    setUser(null)
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +22,16 @@ const App = () => {
     )  
   }, [])
 
+  if(user === null) {
+    return(<div>
+      <LoginForm successCallback={callback} />
+    </div>)
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <div className="user">{user.name} logged in <button onClick={logout}>logout</button></div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
---
+++ part5/blog-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import loginService from "../services/login"
+
+const LoginForm = ({successCallback}) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [message, setMessage] = useState(null)
+
+    const submitForm = async (event) => {
+        event.preventDefault()
+        try {
+            const response = await loginService.login({username, password})
+            setUsername('')
+            setPassword('')
+            successCallback(response)
+        } catch(error) {
+            console.log(error)
+            setMessage(error.message)
+            setTimeout(() => {
+                setMessage(null)
+            }, 5000)                
+        }
+    }
+
+    return(
+        <div>
+            <h2>log in to application</h2>
+            { message ? <div>{message}</div> : <></>}
+            <form onSubmit={submitForm}>
+                <div><span>username</span> <input value={username} onChange={(event) => setUsername(event.target.value)} /></div>
+                <div><span>password</span> <input value={password} onChange={(event) => setPassword(event.target.value)}/></div>
+                <div><button>login</button></div>
+            </form>
+        </div>
+    )    
+}
+
+export default LoginForm
